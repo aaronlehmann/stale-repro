16711927a89613ebdabf4bdecceba0b3d2fff92c
Aaron Lehmann <alehmann@netflix.com>
2022-10-27 10:23:28 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,54 @@
+module github.com/aaronlehmann/stale-repro
+
+go 1.18
+
+require (
+	github.com/moby/buildkit v0.10.5
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+)
+
+require (
+	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/containerd/containerd v1.6.3-0.20220401172941-5ff8fce1fcc6 // indirect
+	github.com/containerd/continuity v0.2.3-0.20220330195504-d132b287edc8 // indirect
+	github.com/containerd/typeurl v1.0.2 // indirect
+	github.com/docker/cli v20.10.13+incompatible // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v20.10.7+incompatible // indirect
+	github.com/go-logr/logr v1.2.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gofrs/flock v0.7.3 // indirect
+	github.com/gogo/googleapis v1.4.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/moby/patternmatcher v0.5.0 // indirect
+	github.com/moby/sys/signal v0.6.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.3-0.20211202183452-c5a74bcca799 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/tonistiigi/fsutil v0.0.0-20220115021204-b19f7f9cb274 // indirect
+	github.com/tonistiigi/units v0.0.0-20180711220420-6950e57a87ea // indirect
+	github.com/tonistiigi/vt100 v0.0.0-20210615222946-8066bb97264f // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.29.0 // indirect
+	go.opentelemetry.io/otel v1.4.1 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.4.1 // indirect
+	go.opentelemetry.io/otel/sdk v1.4.1 // indirect
+	go.opentelemetry.io/otel/trace v1.4.1 // indirect
+	go.opentelemetry.io/proto/otlp v0.12.0 // indirect
+	golang.org/x/crypto v0.0.0-20211202192323-5770296d904e // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
+
+replace github.com/docker/docker => github.com/docker/docker v20.10.3-0.20221006005007-99aa9bb766b5+incompatible // 22.06 branch (v22.06-dev)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/moby/buildkit/client"
+	_ "github.com/moby/buildkit/client/connhelper/dockercontainer"
+	"github.com/moby/buildkit/client/llb"
+	"github.com/moby/buildkit/util/appdefaults"
+	"github.com/moby/buildkit/util/progress/progresswriter"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	addr := os.Getenv("BUILDKIT_HOST")
+	if addr == "" {
+		addr = appdefaults.Address
+	}
+
+	go func() {
+		if err := pushAndRun(context.Background(), addr); err != nil {
+			fmt.Fprintf(os.Stderr, "err: %s\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	time.Sleep(10 * time.Second)
+	if err := pushAndRun(context.Background(), addr); err != nil {
+		fmt.Fprintf(os.Stderr, "err: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+const imageName = "localhost:5000/alehmann/dummy:test"
+
+func pushAndRun(ctx context.Context, addr string) error {
+	if err := push(ctx, addr); err != nil {
+		return err
+	}
+	return run(ctx, addr)
+}
+
+func push(ctx context.Context, addr string) error {
+	st := llb.Image("alpine", llb.LinuxAmd64)
+	st = st.Run(
+		llb.Shlex(`mkdir /files && touch /files/` + strconv.Itoa(rand.Int())),
+	).Root()
+
+	def, err := st.Marshal(ctx)
+	if err != nil {
+		return err
+	}
+
+	c, err := client.New(ctx, addr, client.WithFailFast())
+	if err != nil {
+		return err
+	}
+
+	pw, err := progresswriter.NewPrinter(context.TODO(), os.Stderr, "plain")
+	if err != nil {
+		return err
+	}
+	mw := progresswriter.NewMultiWriter(pw)
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		solveOpt := client.SolveOpt{
+			Exports: []client.ExportEntry{{
+				Type: client.ExporterImage,
+				Attrs: map[string]string{
+					"name": imageName,
+					"push": "true",
+				},
+			}},
+		}
+		_, err := c.Solve(ctx, def, solveOpt, progresswriter.ResetTime(mw.WithPrefix("", false)).Status())
+		return err
+	})
+
+	eg.Go(func() error {
+		<-pw.Done()
+		return pw.Err()
+	})
+
+	return eg.Wait()
+}
+
+func run(ctx context.Context, addr string) error {
+	st := llb.Image(imageName, llb.LinuxAmd64)
+	st = st.Run(
+		llb.Shlex(`find /files`),
+	).Root()
+	st = st.Run(
+		llb.Shlex(`sleep infinity`),
+	).Root()
+
+	def, err := st.Marshal(ctx)
+	if err != nil {
+		return err
+	}
+
+	c, err := client.New(ctx, addr, client.WithFailFast())
+	if err != nil {
+		return err
+	}
+
+	pw, err := progresswriter.NewPrinter(context.TODO(), os.Stderr, "plain")
+	if err != nil {
+		return err
+	}
+	mw := progresswriter.NewMultiWriter(pw)
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		solveOpt := client.SolveOpt{}
+
+		_, err := c.Solve(ctx, def, solveOpt, progresswriter.ResetTime(mw.WithPrefix("", false)).Status())
+		return err
+	})
+
+	eg.Go(func() error {
+		<-pw.Done()
+		return pw.Err()
+	})
+
+	return eg.Wait()
+}
